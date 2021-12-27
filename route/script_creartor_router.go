c6e1eebfb1504badf62022d62224869aa5f77fbd
weiminli <151250082@smail.nju.edu.cn>
2021-12-27 22:48:08 +0800

    add upload url

--- route/script_creartor_router.go
+++ route/script_creartor_router.go
@@ -1,14 +1,19 @@
 package route
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/littlelittlelittleboy/scriptkilling/common/response"
+	"github.com/littlelittlelittleboy/scriptkilling/constants"
 	"github.com/littlelittlelittleboy/scriptkilling/controller"
 )
 
 type ScriptCreatorRouter struct{}
 
 func (r *ScriptCreatorRouter) Init(group *gin.RouterGroup) {
+	group.StaticFS(constants.FILE_BASE_PATH, http.Dir(constants.FILE_BASE_PATH))
+
 	apiGroup := group.Group("/api/admin")
 	{
 		fileUpload := controller.Instance.FileUpload
