ac0cbce172bbc74bef0625aec5baf856ef169e42
weiminli <151250082@smail.nju.edu.cn>
2021-12-27 22:05:04 +0800

    fix:chmod in unix

--- service/file.go
+++ service/file.go
@@ -7,6 +7,7 @@ import (
 	"mime/multipart"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/littlelittlelittleboy/scriptkilling/constants"
 	uuid "github.com/satori/go.uuid"
@@ -16,11 +17,14 @@ type FileService struct{}
 
 func (service *FileService) Save(file multipart.File, header *multipart.FileHeader, scriptName string) (string, constants.UploadFileStatus) {
 	fileExt := path.Ext(header.Filename)
-	fileName := uuid.NewV4().String()
+	fileName := strings.ReplaceAll(uuid.NewV4().String(), "-", "_")
 
-	fileBasePath := fmt.Sprintf("%s/%s", constants.FILE_BASE_PATH, scriptName)
+	fileBasePath := fmt.Sprintf("%s/%s", constants.FILE_BASE_PATH, strings.ReplaceAll(scriptName, "/", "_"))
 	if _, err := os.Stat(fileBasePath); err != nil {
-		os.MkdirAll(fileBasePath, 0644)
+		err := os.MkdirAll(fileBasePath, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	filePath := fmt.Sprintf("%s/%s%s", fileBasePath, fileName, fileExt)
